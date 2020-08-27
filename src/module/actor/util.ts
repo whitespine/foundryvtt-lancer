5b75502500c52f925756a1848c2a4ccec486813d
whitespine <jakehenry.tn@gmail.com>
2020-08-27 02:39:39 -0400

    Fixed rank imports, maybe

--- src/module/actor/util.ts
+++ src/module/actor/util.ts
@@ -2,7 +2,8 @@ import * as mm from "machine-mind";
 import { LancerActor, lancerActorInit } from "./lancer-actor";
 import { CompendiumCategory, store, CompendiumItem } from "machine-mind";
 import { LancerPilotData, LancerPilotActorData } from "../interfaces";
-import { MachineMind_pilot_to_VTT_items_compendium_lookup } from "../item/util";
+import { MachineMind_pilot_to_VTT_items_compendium_lookup, ItemManifest, ItemDataManifest } from "../item/util";
+import { LancerItem, LancerItemData } from "../item/lancer-item";
 
 export async function import_pilot_by_code(code: string): Promise<mm.Pilot> {
   let data = await mm.loadPilot(code);
@@ -36,11 +37,27 @@ export async function update_pilot(pilot: LancerActor, cc_pilot: mm.Pilot): Prom
     // Escape
     return;
   }
-  for (let i of items.items) {
-    await pilot.createOwnedItem(duplicate(i));
+
+  // Do some pre-editing before owning
+  let item_data = items.items.map(i => duplicate(i.data));
+  let item_data_sorted = new ItemDataManifest().add_items(item_data);
+
+  for(let talent of item_data_sorted.talents) {
+    let corr_talent_rank = cc_pilot.getTalentRank(talent.data.id);
+    talent.data.rank = corr_talent_rank;
   }
 
-  // Get data aliases
+  for(let skill of item_data_sorted.talents) {
+    let corr_skill_rank = cc_pilot.getTalentRank(skill.data.id);
+    skill.data.rank = corr_skill_rank;
+  }
+  
+  // Copy them all
+  for (let i of item_data) {
+    await pilot.createOwnedItem(i);
+  }
+
+  // Get actor data for modification. pd is just a shorthand
   let pad = duplicate(pilot.data) as LancerPilotActorData;
   let pd = pad.data;
 
@@ -58,9 +75,12 @@ export async function update_pilot(pilot: LancerActor, cc_pilot: mm.Pilot): Prom
   pd.pilot.cloud_owner_code = cc_pilot.CloudOwnerID;
 
   // Stats
+  console.log("EVADE BEFORE" + pd.pilot.stats.evasion);
   pd.pilot.stats.armor = cc_pilot.Armor;
   pd.pilot.stats.edef = cc_pilot.EDefense;
   pd.pilot.stats.evasion = cc_pilot.Evasion;
+  console.log("EVADE AFTER" + pd.pilot.stats.evasion);
+  console.log(cc_pilot);
   pd.pilot.stats.hp.max = cc_pilot.MaxHP;
   pd.pilot.stats.hp.value = cc_pilot.CurrentHP;
   pd.pilot.stats.size = 0.5;
